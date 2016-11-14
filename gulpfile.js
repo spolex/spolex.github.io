0a486fe932425a4eb0e9182731aebdfcc7482a7a
spolex <spolex@msn.com>
2016-11-14 19:03:29 +0100

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,14 @@
 var gulp        = require('gulp');
 var deploy      = require('gulp-gh-pages');
 
+var options = {
+    "branch" : "master"
+}
+
 /**
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
     return gulp.src("./dist/**/*")
-        .pipe(deploy())
+        .pipe(deploy(options))
 });
